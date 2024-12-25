3c4511bf4d25cdda1701df5006abb2939fcbe1cb
thutasann <thutasann2002@gmail.com>
2024-12-26 02:43:02 +0800

    chore: rename

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	root: 'examples/counter-app',
+	root: 'examples/revo-app',
 	build: {
 		outDir: '../../dist',
 		lib: {
