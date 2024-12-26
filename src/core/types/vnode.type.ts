eb4979f8eafa61221ad35c546884310ca12de086
thutasann <thutasann2002@gmail.com>
2024-12-26 21:54:04 +0800

    refactor: remove hook folder

--- src/core/types/vnode.type.ts
+++ src/core/types/vnode.type.ts
@@ -3,11 +3,29 @@
  * It represents a virtual DOM node.
  */
 export type VNode = {
+	/**
+	 * The type of the VNode.
+	 */
 	type: string | Function;
+	/**
+	 * The properties of the VNode.
+	 */
 	props: Props;
+	/**
+	 * The key of the VNode.
+	 */
 	key: string | null;
+	/**
+	 * The ref of the VNode.
+	 */
 	ref: any;
+	/**
+	 * The DOM node of the VNode.
+	 */
 	_dom?: DOMNode;
+	/**
+	 * The rendered VNode of the VNode.
+	 */
 	_rendered?: VNode;
 };
 
@@ -20,7 +38,13 @@ export type Props = Record<string, any> & { children?: VNode[] };
  * DOMElement is the type for the DOM element.
  */
 export interface DOMElement extends HTMLElement {
+	/**
+	 * The VNode of the DOM element.
+	 */
 	_vnode?: VNode;
+	/**
+	 * The key of the DOM element.
+	 */
 	[key: string]: any;
 }
 
