1c160ca9a2d7043e5c832b9771318b546839ead8
thutasann <thutasann2002@gmail.com>
2024-12-26 23:48:23 +0800

    refactor(development): example app

--- examples/nexora-app/components/counter.tsx
+++ examples/nexora-app/components/counter.tsx
@@ -1,16 +1,30 @@
 import { createState, Nexora } from '../../../dist';
 
-const Counter = () => {
-	console.log('Counter');
+export const Counter = () => {
 	const [getCount, setCount] = createState(0);
+	console.log('Counter --> ', getCount());
 
 	return (
 		<section>
-			<h2>Count: {getCount()}</h2>
-			<button onclick={() => setCount(getCount() + 1)}>Increment</button>
-			<button onclick={() => setCount(getCount() - 1)}>Decrement</button>
+			<h2>Current Count: {getCount()}</h2>
+			<button
+				onclick={() => {
+					setCount((prev) => {
+						return prev + 1;
+					});
+				}}
+			>
+				Increment
+			</button>
+			<button
+				onclick={() => {
+					setCount((prev) => {
+						return prev - 1;
+					});
+				}}
+			>
+				Decrement
+			</button>
 		</section>
 	);
 };
-
-export default Counter;
--- examples/nexora-app/index.tsx
+++ examples/nexora-app/index.tsx
@@ -1,48 +1,14 @@
-import { createState, Nexora, reactive, render } from '../../dist';
-
-const Counter = () => {
-	const [getCount, setCount] = createState(0);
-	console.log('Counter --> ', getCount());
-
-	return (
-		<section>
-			<h2>Current Count: {getCount()}</h2>
-			<button
-				onclick={() => {
-					setCount((prev) => {
-						return prev + 1;
-					});
-				}}
-			>
-				Increment
-			</button>
-			<button
-				onclick={() => {
-					setCount((prev) => {
-						return prev - 1;
-					});
-				}}
-			>
-				Decrement
-			</button>
-		</section>
-	);
-};
-
-const Header = () => {
-	return <h1>Nexora App</h1>;
-};
+import { Nexora, reactive, render } from '../../dist';
+import { Counter } from './components/counter';
 
 function App() {
 	return (
 		<div>
-			<Header />
+			<h1>Nexora App</h1>
 			<Counter />
 		</div>
 	);
 }
 
 const app = document.getElementById('app');
-if (app) {
-	render(reactive.renderComponent(App), app);
-}
+if (app) render(reactive.renderComponent(App), app);
