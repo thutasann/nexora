db795b634587b219a3766414203475f902429563
thutasann <thutasann2002@gmail.com>
2024-12-29 01:23:27 +0800

    add(examples): on init life cycle data fetch

--- examples/nexora-app/components/lifecycles-related/on-init-sample.tsx
+++ examples/nexora-app/components/lifecycles-related/on-init-sample.tsx
@@ -6,8 +6,25 @@ export function OnInitSample() {
   });
 
   return (
+    <div>
+      <div className='container'>
+        <h2>OnInitSample</h2>
+      </div>
+      <OnInitSample2 />
+    </div>
+  );
+}
+
+export function OnInitSample2() {
+  onInit(async () => {
+    const data = await fetch('https://jsonplaceholder.typicode.com/todos');
+    const json = await data.json();
+    console.log('OnInitSample2 ==> ', json);
+  });
+
+  return (
     <div className='container'>
-      <h2>OnInitSample</h2>
+      <h2>OnInitSample2</h2>
     </div>
   );
 }
